8fd6548709c48a7f5da6251c5eb372d154e5b661
endor <fp@notjusthosting.com>
2010-09-26 18:36:33 +0200

    implement basic recursive algorithm to find enclosed stones and destroy them

--- models/stone.js
+++ models/stone.js
@@ -1,26 +1,111 @@
-var sys = require('sys')
+Stone = function(options) {
+  var stone = this;
 
-StoneService = function() {}
+  ['x', 'y', 'user', 'game'].forEach(function(attr) {
+    stone[attr] = options[attr]
+  })
+  
+  stone.id = stone.x + '' + stone.y
+  stone.value = stone.game.board[stone.x][stone.y]
+}
 
-StoneService.prototype.validate = function(options) {
-  var errors = [], game = options.game, user = options.user, x = options.x,
-      y = options.y, history = game.history
-      
-  if(game.board[x][y] != 0) { errors.push("There's already a stone on this field.") }
-  if(history.length == 0 && user.identifier != game.white.identifier) { errors.push("It's not your turn.") }
-  if(history.length > 0  && user.identifier == history[history.length - 1].identifier) { errors.push("It's not your turn.") }
+Stone.prototype.directions = ['east', 'south', 'north', 'west']
+
+Stone.prototype.validate = function() {
+  var errors = [], history = this.game.history
+
+  if(this.game.board[this.x][this.y] !== 0) { errors.push("There's already a stone on this field.") }
+  if(history.length === 0 && this.user.identifier !== this.game.white.identifier) { errors.push("It's not your turn.") }
+  if(history.length > 0  && this.user.identifier === history[history.length - 1].identifier) { errors.push("It's not your turn.") }
   
   return errors
 }
 
-StoneService.prototype.set = function(options) {
-  var game = options.game, user = options.user, value = 1, color = 'white',
-      x = options.x, y = options.y
-      
-  if(user.identifier == game.black.identifier) { value = 2; color = 'black' }
+Stone.prototype.set = function(options) {
+  var value = 1, color = 'white', stone = this
   
-  game.board[x][y] = value
-  game.history.push({x: x, y: y, identifier: user.identifier, color: color})
+  if(this.user.identifier === this.game.black.identifier) { value = 2; color = 'black' }
+  
+  this.game.board[this.x][this.y] = value
+  this.game.history.push({x: this.x, y: this.y, identifier: this.user.identifier, color: color})
+  
+  this.directions.forEach(function(direction) {
+    var _stone = stone[direction]()
+    if(_stone && _stone.value !== value && _stone.value !== 0) {
+      if(!stone.free(_stone, [])) {
+        stone.destroy(_stone, [])
+      }
+      console.log(direction, color, stone.id, stone.free(_stone, []))
+    }
+  })
 }
 
-exports.StoneService = StoneService
+Stone.prototype.direction = function(callback, x, y) {
+  if(callback.call(this)) {
+    var value = this.game.board[x][y],
+      user = null
+  
+    if(value === 1) { user = this.game.white }
+    if(value === 2) { user = this.game.black }
+
+    return new Stone({ game: this.game, user: user, x: x, y: y })
+  } else {
+    return null
+  }  
+}
+
+Stone.prototype.north = function() {
+  return this.direction(function() { return this.y - 1 >= 0 }, this.x, this.y - 1)
+}
+
+Stone.prototype.south = function() {
+  return this.direction(function() { return this.y + 1 < this.game.board_size }, this.x, this.y + 1)
+}
+
+Stone.prototype.east = function() {
+  return this.direction(function() { return this.x - 1 >= 0 }, this.x - 1, this.y)  
+}
+
+Stone.prototype.west = function() {
+  return this.direction(function() { return this.x + 1 < this.game.board_size }, this.x + 1, this.y)
+}
+
+Stone.prototype.free = function(stone, already_looked_up) {
+  var free = false
+
+  already_looked_up.push(stone.id);
+  
+  this.directions.forEach(function(direction) {
+    var _stone = stone[direction]()
+    
+    if(!_stone) { return }
+
+    if(_stone.user && _stone.user.identifier === stone.user.identifier
+        && already_looked_up.indexOf(_stone.id) < 0) {
+      free = free || stone.free(_stone, already_looked_up)
+    } else if(_stone.game.board[_stone.x][stone.y] === 0) {
+      free = free || true
+    }        
+  })
+  
+  return free
+}
+
+Stone.prototype.destroy = function(stone, already_destroyed) {
+  already_destroyed.push(stone.id)
+  
+  this.directions.forEach(function(direction) {
+    var _stone = stone[direction]()
+    
+    if(!_stone) { return }
+    
+    if(_stone.user && _stone.user.identifier === stone.user.identifier
+        && already_destroyed.indexOf(_stone.id) < 0) {
+      _stone.destroy(_stone, already_destroyed)
+    }
+  })
+  
+  stone.game.board[stone.x][stone.y] = 0
+}
+
+exports.Stone = Stone
---
+++ spec/stone_spec.js
@@ -0,0 +1,87 @@
+describe 'Stone'
+  before_each
+    Game = new GameService(), User = new UserService()
+    
+    black = { identifier: '456' }
+    white = { identifier: '123' }
+    game = Game.save({board_size: 9, white: white, black: black})
+  end
+  
+  describe 'validate'
+    it 'should return an error if there\'s already a stone on the field'
+      stone = new Stone({ game: game, user: white, x: 0, y: 0 })
+      stone.set()
+      stone = new Stone({ game: game, user: black, x: 0, y: 0 })
+      errors = stone.validate()
+      errors[0].should.equal 'There\'s already a stone on this field.'
+    end
+    
+    it 'should return an error if the same player tries to set again'
+      stone = new Stone({ game: game, user: white, x: 0, y: 0 })
+      stone.set()
+      stone = new Stone({ game: game, user: white, x: 0, y: 1 })
+      errors = stone.validate()
+      errors[0].should.equal 'It\'s not your turn.'
+    end
+  end
+  
+  describe 'set'
+    it 'should not remove undead enemy stones'
+      stone = new Stone({ game: game, user: white, x: 0, y: 0 })
+      stone.set()
+      stone = new Stone({ game: game, user: black, x: 0, y: 1 })
+      stone.set()
+      game.board[0][0].should.equal 1
+    end
+  
+    it 'should remove dead enemy stones from a corner'
+      /*
+      |--------
+      | w b
+      | b w
+      |
+      */
+      [
+        { user: white, x: 0, y: 0 },
+        { user: black, x: 0, y: 1 },
+        { user: white, x: 1, y: 1 },
+        { user: black, x: 1, y: 0 }
+      ].forEach(function(move) {
+        stone = new Stone({ game: game, user: move.user, x: move.x, y: move.y })
+        stone.set()
+      });
+      
+      game.board[0][0].should.equal 0
+    end
+    
+    it 'should remove dead enemy stones from the middle'
+      /*
+        |------------
+        |
+        |   w b
+        | w b w b b
+        | w b w
+        |   w
+        |
+      */
+      [
+        { user: white, x: 1, y: 1 },
+        { user: black, x: 1, y: 2 },
+        { user: white, x: 0, y: 2 },
+        { user: black, x: 1, y: 3 },
+        { user: white, x: 0, y: 3 },
+        { user: black, x: 2, y: 1 },
+        { user: white, x: 1, y: 4 },
+        { user: black, x: 3, y: 2 },
+        { user: white, x: 2, y: 3 },
+        { user: black, x: 4, y: 2 },
+        { user: white, x: 2, y: 2 }
+      ].forEach(function(move) {
+        stone = new Stone({ game: game, user: move.user, x: move.x, y: move.y })
+        stone.set()
+      })
+      game.board[1][2].should.equal 0
+      game.board[1][3].should.equal 0
+    end    
+  end
+end
